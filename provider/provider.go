d11b20c295459d9901ecaf94c8f4b9f540e88337
Benoit Sigoure <tsuna@arista.com>
2016-02-25 15:56:25 -0800

    Licensing: mention the EULA in all our source files.
    Except in the sysdb/ and smash/ packages since these
    will never be shared in source form.
    
    This change was requested by our legal team (Abraham
    Mertens and Bill Yee), after revising our EULA to
    more explicitly cover "extras" like TerminAttr.
    
    This change was generated by doing:
      gsed -i 's!^\(//\|#\) Arista Networks, Inc. Confidential.*!&\
      \1 Subject to Arista Networks, Inc.'\''s EULA.\
      \1 FOR INTERNAL USE ONLY. NOT FOR DISTRIBUTION.!' <input files>
    
    Change-Id: Iba9b4991d69d193e9460782eb4cfaf36775653a7

--- provider/provider.go
+++ provider/provider.go
@@ -1,5 +1,7 @@
 // Copyright (c) 2015 Arista Networks, Inc.  All rights reserved.
 // Arista Networks, Inc. Confidential and Proprietary.
+// Subject to Arista Networks, Inc.'s EULA.
+// FOR INTERNAL USE ONLY. NOT FOR DISTRIBUTION.
 
 package provider
 
