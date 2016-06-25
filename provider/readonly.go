44cf36bb7e895561c86f706f133dad050208a527
Benoit Sigoure <tsuna@arista.com>
2016-06-25 01:03:20 -0700

    provider: Add a helper struct for read-only providers.
    Change-Id: I28e3ddb7b6454616a85524b00c116fd86bccdbeb

---
+++ provider/readonly.go
@@ -0,0 +1,33 @@
+// Copyright (c) 2016 Arista Networks, Inc.  All rights reserved.
+// Arista Networks, Inc. Confidential and Proprietary.
+// Subject to Arista Networks, Inc.'s EULA.
+// FOR INTERNAL USE ONLY. NOT FOR DISTRIBUTION.
+
+package provider
+
+import (
+	"arista/schema"
+	"arista/types"
+	"fmt"
+
+	"github.com/aristanetworks/goarista/key"
+)
+
+// ReadOnly helps implement the update methods of a read-only provider.
+type ReadOnly struct{}
+
+// Write always fails.
+func (ro ReadOnly) Write(notif types.Notification) error {
+	return fmt.Errorf("cannot write to %s: path is read-only", notif.Path())
+}
+
+// InstantiateChild always fails.
+func (ro ReadOnly) InstantiateChild(child types.Entity, attrDef *schema.AttrDef,
+	k key.Key, ctorArgs map[string]interface{}) error {
+	return fmt.Errorf("cannot instantiate %s: parent entity is read-only", child.Path())
+}
+
+// DeleteChild always fails.
+func (ro ReadOnly) DeleteChild(child types.Entity, attrDef *schema.AttrDef, k key.Key) error {
+	return fmt.Errorf("cannot delete %s: parent entity is read-only", child.Path())
+}
