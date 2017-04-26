f9c93862c873fc241a44ccb714b9fdc360748814
ren <ren@arista.com>
2017-04-26 12:31:49 -0700

    Adding to nightly Ansible runs that ensures prelink is removed from affected servers. This is a fix prelink does not work with FIPS mode enabled
    Change-Id: I98db4a4ef1a2ea39527279ab88ca0579eba20f83

--- AroraConfig/playbooks/test/test_packages.sh
+++ AroraConfig/playbooks/test/test_packages.sh
@@ -1,1 +1,5 @@
+# Tests packages are installed
 rpm -qa telegraf-Linux | grep telegraf-Linux
+
+# Tests packages are removed
+! rpm -qa prelink | grep prelink
--- AroraConfig/playbooks/test/test_postfix.py
+++ AroraConfig/playbooks/test/test_postfix.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright (c) 2016 Arista Networks, Inc.  All rights reserved.
+# Copyright (c) 2017 Arista Networks, Inc.  All rights reserved.
 # Arista Networks, Inc. Confidential and Proprietary.
 
 print "This test is empty"
