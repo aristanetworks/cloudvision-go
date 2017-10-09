470cbdab850c1bd0cb2c9cfcb5561e94c81405da
Fabrice Rabaute <fabrice@arista.com>
2017-10-09 15:10:21 -0700

    ops/ansible: Add deploy.sh helper script to run ansible
    This script is simplifying the usage of "ansible-playbook"
    command, and it takes care of validating the cluster name
    as well as passing the correct args for the inventory and
    the vault password.
    
    Vault management will come in subsequent commits
    
    Change-Id: Ieaf5846c4441ed07bd494c89d902a2afcc3a9c48

---
+++ ops/ansible/deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Copyright (c) 2015 Arista Networks, Inc.  All rights reserved.
+# Arista Networks, Inc. Confidential and Proprietary.
+# Subject to Arista Networks, Inc.'s EULA.
+# FOR INTERNAL USE ONLY. NOT FOR DISTRIBUTION.
+
+# This script will deploy a cluster
+
+# The first argument is mandatory: It's the name of the cluster (dev, staging, etc)
+# This name is the folder name in the "inventories" folder
+
+# The other args will be passed to "ansible-playbook"
+
+set -ex
+
+ROOTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CLUSTER=$1
+
+source ./util.sh
+
+check_cluster_name "$CLUSTER"
+
+shift
+
+ansible-playbook \
+	playbook.yml \
+	-i "$ROOTDIR/inventories/$CLUSTER/hosts" \
+	--vault-password-file "$ROOTDIR/.pass.$CLUSTER" \
+	"$@"
