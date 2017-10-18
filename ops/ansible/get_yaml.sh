5c660e9ff41cd40ee0fe0b3b72c7821c13412eef
Fabrice Rabaute <fabrice@arista.com>
2017-10-18 12:14:15 -0700

    ops/ansible: Add checks on get_yaml.sh script
    Change-Id: I95f6e6107d53a37ee28f50c3b9d2451f6adb5c6d

--- ops/ansible/get_yaml.sh
+++ ops/ansible/get_yaml.sh
@@ -6,8 +6,19 @@
 # $1 is the cluster, either "dev" or "staging"
 # $2 is the kubernetes task name for which you want to output the yaml
 
+source ./util.sh
+
+CLUSTER=$1
+
+check_cluster_name "$CLUSTER"
+
+if [ -z "$2" ]; then
+	echo "Task name not provided"
+	exit 1
+fi
+
 DISPLAY_K8S_YAML="$2" ansible-playbook \
-	-i "inventories/$1/hosts" \
+	-i "inventories/$CLUSTER/hosts" \
 	-l localhost \
 	--start-at-task \
 	"$2" \
