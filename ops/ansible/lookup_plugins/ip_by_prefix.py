cbc0c4b46e715f35601684b19c95b84713f5fe5d
Fabrice Rabaute <fabrice@arista.com>
2017-11-15 09:34:20 -0800

    ops/ansible-ip_by_prefix: Allow to pass host info as second arg
    For some tasks, the ip to retrieve is not the one being currently
    used.
    This change allows to pass the host info as arg so ip_by_prefix
    can be used to retrieve ip of a different host.
    
    Change-Id: I9bf8f16f3b64ee7a64d252c75df9071769b39899

--- ops/ansible/lookup_plugins/ip_by_prefix.py
+++ ops/ansible/lookup_plugins/ip_by_prefix.py
@@ -10,15 +10,19 @@ class LookupModule(LookupBase):
 
     def run(self, terms, variables, **kwargs):
 
-    	if len(terms) != 1:
-    		raise AnsibleError("ip_by_prefix requires one argument only")
+        if len(terms) not in [1, 2]:
+            raise AnsibleError("ip_by_prefix requires one or two arguments only")
+
+        prefix = terms[0]
+        if len(terms) == 2:
+            variables = terms[1]
 
     	for a in variables["ansible_all_ipv4_addresses"]:
-    		if a.startswith(terms[0]):
+    		if a.startswith(prefix):
     			return [a]
 
     	for a in variables["ansible_all_ipv6_addresses"]:
-    		if a.startswith(terms[0]):
+    		if a.startswith(prefix):
     			return [a]
 
-        raise AnsibleError("Unable to find ip address with prefix %s in list %s" % (terms[0], variables["ansible_all_ipv4_addresses"]))
+        raise AnsibleError("Unable to find ip address with prefix %s in list %s" % (prefix, variables["ansible_all_ipv4_addresses"]))
