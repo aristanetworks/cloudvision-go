80032f56e603614703001a8d5d2a7a6b34476c8e
Fabrice Rabaute <fabrice@arista.com>
2017-11-15 17:29:48 -0800

    ops/ansible: Switch clusters to new certs
    Staging certs are still the old certs as this cluster
    has not been migrated yet.
    
    Change-Id: I3f1e6bda06adf1b1832de95888860cdd345a2322

--- ops/init-kube-client.sh
+++ ops/init-kube-client.sh
@@ -4,28 +4,52 @@
 
 ROOTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+mkdir -p "$HOME/.kube/certs/dev"
+
+# Decrypt certificates for dev cluster
+for f in ca admin-key admin;
+do
+	ansible-vault \
+		--vault-password-file "$ROOTDIR/ansible/.pass.dev" \
+		--output="$HOME/.kube/certs/dev/${f}.pem" \
+		decrypt \
+		"$ROOTDIR/ansible/inventories/dev/files/k8s/${f}.pem"
+done
+
 # Setup dev cluster
 kubectl config set-cluster dev \
 	--server=https://master.dev-infra.corp.arista.io \
-	--certificate-authority="$ROOTDIR/ansible/inventories/dev/files/ca.pem"
+	--certificate-authority="$HOME/.kube/certs/dev/ca.pem"
 kubectl config set-credentials dev \
-	--certificate-authority="$ROOTDIR/ansible/inventories/dev/files/ca.pem" \
-	--client-key="$ROOTDIR/ansible/inventories/dev/files/admin-key.pem" \
-	--client-certificate="$ROOTDIR/ansible/inventories/dev/files/admin.pem"
+	--certificate-authority="$HOME/.kube/certs/dev/ca.pem" \
+	--client-key="$HOME/.kube/certs/dev/admin-key.pem" \
+	--client-certificate="$HOME/.kube/certs/dev/admin.pem"
 kubectl config set-context dev \
 	--cluster=dev \
 	--user=dev
 # Let's have dev the default context for now
 kubectl config use-context dev
 
+mkdir -p "$HOME/.kube/certs/staging"
+
+# Decrypt certificates for staging cluster
+for f in ca admin-key admin;
+do
+	ansible-vault \
+		--vault-password-file "$ROOTDIR/ansible/.pass.staging" \
+		--output="$HOME/.kube/certs/staging/${f}.pem" \
+		decrypt \
+		"$ROOTDIR/ansible/inventories/staging/files/k8s/${f}.pem"
+done
+
 # Setup staging cluster
 kubectl config set-cluster staging \
 	--server=https://master.staging-infra.corp.arista.io \
-	--certificate-authority="$ROOTDIR/ansible/inventories/staging/files/ca.pem"
+	--certificate-authority="$HOME/.kube/certs/staging/ca.pem"
 kubectl config set-credentials staging \
-	--certificate-authority="$ROOTDIR/ansible/inventories/staging/files/ca.pem" \
-	--client-key="$ROOTDIR/ansible/inventories/staging/files/admin-key.pem" \
-	--client-certificate="$ROOTDIR/ansible/inventories/staging/files/admin.pem"
+	--certificate-authority="$HOME/.kube/certs/staging/ca.pem" \
+	--client-key="$HOME/.kube/certs/staging/admin-key.pem" \
+	--client-certificate="$HOME/.kube/certs/staging/admin.pem"
 kubectl config set-context staging \
 	--cluster=staging \
 	--user=staging
