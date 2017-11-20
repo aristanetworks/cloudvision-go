62bc40faaf212f5781efa0cd5acdf8bcdb808cde
Fabrice Rabaute <fabrice@arista.com>
2017-11-20 13:35:09 -0800

    ops/ansible: Improve init-kube-client.sh script to handle any cluster
    This script can now take an argument which is the cluster name
    and configure it on the local machine.
    
    Change-Id: Ic768752ef519ee0acd7e415f200b07abd67ea40f

--- ops/init-kube-client.sh
+++ ops/init-kube-client.sh
@@ -2,56 +2,61 @@
 # Copyright (c) 2017 Arista Networks, Inc.  All rights reserved.
 # Arista Networks, Inc. Confidential and Proprietary.
 
+set -e
+
 ROOTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-mkdir -p "$HOME/.kube/certs/dev"
+NARGS=$#
 
-# Decrypt certificates for dev cluster
-for f in ca admin-key admin;
-do
-	ansible-vault \
-		--vault-password-file "$ROOTDIR/ansible/.pass.dev" \
-		--output="$HOME/.kube/certs/dev/${f}.pem" \
-		decrypt \
-		"$ROOTDIR/ansible/inventories/dev/files/k8s/${f}.pem"
+if [ "$NARGS" -eq 0 ]; then
+	clusters="dev staging"
+else
+	clusters=$@
+fi
+
+initcluster() {
+	local CLUSTER=$1
+
+	# Create folder to save certs for this cluster
+	mkdir -p "$HOME/.kube/certs/$CLUSTER"
+
+	# Decrypt certificates for the cluster
+	for f in ca admin-key admin;
+	do
+		ansible-vault \
+			--vault-password-file "$ROOTDIR/ansible/.pass.$CLUSTER" \
+			--output="$HOME/.kube/certs/$CLUSTER/${f}.pem" \
+			decrypt \
+			"$ROOTDIR/ansible/inventories/$CLUSTER/files/k8s/${f}.pem"
+	done
+
+	# Setup kubectl config for the cluster
+	local SERVER
+	case $CLUSTER in
+		"dev"|"staging")
+			SERVER=https://master.${CLUSTER}-infra.corp.arista.io
+			;;
+		*)
+			SERVER=https://127.0.0.1:8240
+			;;
+	esac
+	kubectl config set-cluster "$CLUSTER" \
+		--server=$SERVER \
+		--certificate-authority="$HOME/.kube/certs/$CLUSTER/ca.pem"
+	kubectl config set-credentials "$CLUSTER" \
+		--certificate-authority="$HOME/.kube/certs/$CLUSTER/ca.pem" \
+		--client-key="$HOME/.kube/certs/$CLUSTER/admin-key.pem" \
+		--client-certificate="$HOME/.kube/certs/$CLUSTER/admin.pem"
+	kubectl config set-context "$CLUSTER" \
+		--cluster="$CLUSTER" \
+		--user="$CLUSTER"
+}
+
+for c in $clusters; do
+	initcluster "$c"
 done
 
-# Setup dev cluster
-kubectl config set-cluster dev \
-	--server=https://master.dev-infra.corp.arista.io \
-	--certificate-authority="$HOME/.kube/certs/dev/ca.pem"
-kubectl config set-credentials dev \
-	--certificate-authority="$HOME/.kube/certs/dev/ca.pem" \
-	--client-key="$HOME/.kube/certs/dev/admin-key.pem" \
-	--client-certificate="$HOME/.kube/certs/dev/admin.pem"
-kubectl config set-context dev \
-	--cluster=dev \
-	--user=dev
-# Let's have dev the default context for now
-kubectl config use-context dev
-
-mkdir -p "$HOME/.kube/certs/staging"
-
-# Decrypt certificates for staging cluster
-for f in ca admin-key admin;
-do
-	ansible-vault \
-		--vault-password-file "$ROOTDIR/ansible/.pass.staging" \
-		--output="$HOME/.kube/certs/staging/${f}.pem" \
-		decrypt \
-		"$ROOTDIR/ansible/inventories/staging/files/k8s/${f}.pem"
-done
-
-# Setup staging cluster
-kubectl config set-cluster staging \
-	--server=https://master.staging-infra.corp.arista.io \
-	--certificate-authority="$HOME/.kube/certs/staging/ca.pem"
-kubectl config set-credentials staging \
-	--certificate-authority="$HOME/.kube/certs/staging/ca.pem" \
-	--client-key="$HOME/.kube/certs/staging/admin-key.pem" \
-	--client-certificate="$HOME/.kube/certs/staging/admin.pem"
-kubectl config set-context staging \
-	--cluster=staging \
-	--user=staging
-# Let's have dev the default context for now
-# kubectl config use-context staging
+if [ "$NARGS" -eq 0 ]; then
+	# Let's have dev the default context for default case
+	kubectl config use-context dev
+fi
