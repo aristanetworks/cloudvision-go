3af181fd0fd94defb54eb6b4795e086f41b680bd
jcr <jcr@arista.com>
2018-11-16 17:13:36 -0800

    provider: Eliminate gopenconfig dependency in SNMP provider
    In preparation for moving the SNMP provider and related helper functions
    into an open-source repo, I've changed the way the SNMP provider maps
    from SNMP-defined type values to OpenConfig strings. We had been using
    mappings defined in gopenconfig/eos and now we just define the mappings
    in the provider package. OpenConfig actually uses the SNMP types, and
    since we're trying to standardize to OpenConfig, they might plausibly be
    used in the future. So I've put the mappings in
    provider/openconfig/openconfig_helpers.go.
    
    The hope is to switch from a provider/providers/<all providers>
    directory structure  to provider/<provider name>/<provider_name>.go for
    cleaner namespacing, etc., so I've started that here.
    
    Change-Id: I44456866c2daeebaebac9216a70dc93086e87446

--- provider/providers/snmp.go
+++ provider/providers/snmp.go
@@ -6,10 +6,8 @@
 package providers
 
 import (
-	"arista/gopenconfig/eos"
-	"arista/gopenconfig/eos/converter"
-	"arista/gopenconfig/eos/mapping"
 	"arista/provider"
+	"arista/provider/openconfig"
 	"bytes"
 	"context"
 	"encoding/hex"
@@ -26,7 +24,6 @@ import (
 )
 
 type snmp struct {
-	provider.ReadOnly
 	errc             chan error
 	ctx              context.Context
 	client           gnmi.GNMIClient
@@ -111,95 +108,6 @@ func oidIndex(oid string) (string, string, error) {
 		return "", "", fmt.Errorf("oid '%s' does not match expected format", oid)
 	}
 	return oid[:finalDotPos], oid[(finalDotPos + 1):], nil
-}
-
-// Return the SNMP interface type string corresponding to the
-// interface type number we receive. OpenConfig uses the same strings
-// as SNMP.
-func ifType(t int) string {
-	ifTypeMap := []string{"", "other", "regular1822", "hdh1822", "ddnX25",
-		"rfc877x25", "ethernetCsmacd", "iso88023Csmacd", "iso88024TokenBus",
-		"iso88025TokenRing", "iso88026Man", "starLan", "proteon10Mbit",
-		"proteon80Mbit", "hyperchannel", "fddi", "lapb", "sdlc", "ds1",
-		"e1", "basicISDN", "primaryISDN", "propPointToPointSerial", "ppp",
-		"softwareLoopback", "eon", "ethernet3Mbit", "nsip", "slip", "ultra",
-		"ds3", "sip", "frameRelay", "rs232", "para", "arcnet", "arcnetPlus",
-		"atm", "miox25", "sonet", "x25ple", "iso88022llc", "localTalk",
-		"smdsDxi", "frameRelayService", "v35", "hssi", "hippi", "modem",
-		"aal5", "sonetPath", "sonetVT", "smdsIcip", "propVirtual",
-		"propMultiplexor", "ieee80212", "fibreChannel", "hippiInterface",
-		"frameRelayInterconnect", "aflane8023", "aflane8025", "cctEmul",
-		"fastEther", "isdn", "v11", "v36", "g703at64k", "g703at2mb", "qllc",
-		"fastEtherFX", "channel", "ieee80211", "ibm370parChan", "escon",
-		"dlsw", "isdns", "isdnu", "lapd", "ipSwitch", "rsrb", "atmLogical",
-		"ds0", "ds0Bundle", "bsc", "async", "cnr", "iso88025Dtr", "eplrs",
-		"arap", "propCnls", "hostPad", "termPad", "frameRelayMPI", "x213",
-		"adsl", "radsl", "sdsl", "vdsl", "iso88025CRFPInt", "myrinet",
-		"voiceEM", "voiceFXO", "voiceFXS", "voiceEncap", "voiceOverIp",
-		"atmDxi", "atmFuni", "atmIma", "pppMultilinkBundle", "ipOverCdlc",
-		"ipOverClaw", "stackToStack", "virtualIpAddress", "mpc",
-		"ipOverAtm", "iso88025Fiber", "tdlc", "gigabitEthernet", "hdlc",
-		"lapf", "v37", "x25mlp", "x25huntGroup", "transpHdlc", "interleave",
-		"fast", "ip", "docsCableMaclayer", "docsCableDownstream",
-		"docsCableUpstream", "a12MppSwitch", "tunnel", "coffee", "ces",
-		"atmSubInterface", "l2vlan", "l3ipvlan", "l3ipxvlan",
-		"digitalPowerline", "mediaMailOverIp", "dtm", "dcn", "ipForward",
-		"msdsl", "ieee1394", "if-gsn", "dvbRccMacLayer", "dvbRccDownstream",
-		"dvbRccUpstream", "atmVirtual", "mplsTunnel", "srp", "voiceOverAtm",
-		"voiceOverFrameRelay", "idsl", "compositeLink", "ss7SigLink",
-		"propWirelessP2P", "frForward", "rfc1483", "usb", "ieee8023adLag",
-		"bgppolicyaccounting", "frf16MfrBundle", "h323Gatekeeper",
-		"h323Proxy", "mpls", "mfSigLink", "hdsl2", "shdsl", "ds1FDL", "pos",
-		"dvbAsiIn", "dvbAsiOut", "plc", "nfas", "tr008", "gr303RDT",
-		"gr303IDT", "isup", "propDocsWirelessMaclayer",
-		"propDocsWirelessDownstream", "propDocsWirelessUpstream",
-		"hiperlan2", "propBWAp2Mp", "sonetOverheadChannel",
-		"digitalWrapperOverheadChannel", "aal2", "radioMAC", "atmRadio",
-		"imt", "mvl", "reachDSL", "frDlciEndPt", "atmVciEndPt",
-		"opticalChannel", "opticalTransport", "propAtm", "voiceOverCable",
-		"infiniband", "teLink", "q2931", "virtualTg", "sipTg", "sipSig",
-		"docsCableUpstreamChannel", "econet", "pon155", "pon622", "bridge",
-		"linegroup", "voiceEMFGD", "voiceFGDEANA", "voiceDID",
-		"mpegTransport", "sixToFour", "gtp", "pdnEtherLoop1",
-		"pdnEtherLoop2", "opticalChannelGroup", "homepna", "gfp",
-		"ciscoISLvlan", "actelisMetaLOOP", "fcipLink", "rpr", "qam", "lmp",
-		"cblVectaStar", "docsCableMCmtsDownstream", "adsl2",
-		"macSecControlledIF", "macSecUncontrolledIF", "aviciOpticalEther",
-		"atmbond", "voiceFGDOS", "mocaVersion1", "ieee80216WMAN",
-		"adsl2plus", "dvbRcsMacLayer", "dvbTdm", "dvbRcsTdma", "x86Laps",
-		"wwanPP", "wwanPP2", "voiceEBS", "ifPwType", "ilan", "pip",
-		"aluELP", "gpon", "vdsl2", "capwapDot11Profile", "capwapDot11Bss",
-		"capwapWtpVirtualRadio", "bits", "docsCableUpstreamRfPort",
-		"cableDownstreamRfPort", "vmwareVirtualNic", "ieee802154", "otnOdu",
-		"otnOtu", "ifVfiType", "g9981", "g9982", "g9983", "aluEpon",
-		"aluEponOnu", "aluEponPhysicalUni", "aluEponLogicalLink",
-		"aluGponOnu", "aluGponPhysicalUni", "vmwareNicTeam"}
-	if t >= len(ifTypeMap) || t == 0 {
-		t = 1
-	}
-	return ifTypeMap[t]
-}
-
-// Return the OpenConfig interface status string corresponding
-// to the SNMP interface status.
-func ifStatus(status int) string {
-	switch uint32(status) {
-	case eos.IntfOperUp().EnumValue():
-		return converter.IntfOperStatusUp
-	case eos.IntfOperDown().EnumValue():
-		return converter.IntfOperStatusDown
-	case eos.IntfOperTesting().EnumValue():
-		return converter.IntfOperStatusTesting
-	case eos.IntfOperUnknown().EnumValue():
-		return converter.IntfOperStatusUnknown
-	case eos.IntfOperDormant().EnumValue():
-		return converter.IntfOperStatusDormant
-	case eos.IntfOperNotPresent().EnumValue():
-		return converter.IntfOperStatusNotPresent
-	case eos.IntfOperLowerLayerDown().EnumValue():
-		return converter.IntfOperStatusLowerLayerDown
-	}
-	return ""
 }
 
 const (
@@ -361,16 +269,16 @@ func (s *snmp) handleInterfacePDU(pdu gosnmp.SnmpPDU) ([]*gnmi.Update, error) {
 			strval(pdu.Value))
 	case snmpIfType:
 		u = update(GNMIIntfStatePath(intfName, "type"),
-			strval(ifType(pdu.Value.(int))))
+			strval(openconfig.InterfaceType(pdu.Value.(int))))
 	case snmpIfMtu:
 		u = update(GNMIIntfStatePath(intfName, "mtu"),
 			uintval(pdu.Value))
 	case snmpIfAdminStatus:
 		u = update(GNMIIntfStatePath(intfName, "admin-status"),
-			strval(ifStatus(pdu.Value.(int))))
+			strval(openconfig.IntfAdminStatus(pdu.Value.(int))))
 	case snmpIfOperStatus:
 		u = update(GNMIIntfStatePath(intfName, "oper-status"),
-			strval(ifStatus(pdu.Value.(int))))
+			strval(openconfig.IntfOperStatus(pdu.Value.(int))))
 	case snmpIfInOctets:
 		u = update(GNMIIntfStateCountersPath(intfName, "in-octets"),
 			uintval(pdu.Value))
@@ -474,50 +382,6 @@ func (s *snmp) updateSystemState() (*gnmi.SetRequest, error) {
 	return setReq, nil
 }
 
-// Return the OpenConfig chassis ID type string corresponding to the
-// SNMP chassis ID type value.
-func lldpChassisIDType(id int) string {
-	switch uint32(id) {
-	case eos.LLDPChassisIDChassisComponent().EnumValue():
-		return mapping.ChassisIDTypeChassisComponent
-	case eos.LLDPChassisIDInterfaceAlias().EnumValue():
-		return mapping.ChassisIDTypeInterfaceAlias
-	case eos.LLDPChassisIDPortComponent().EnumValue():
-		return mapping.ChassisIDTypePortComponent
-	case eos.LLDPChassisIDMacAddress().EnumValue():
-		return mapping.ChassisIDTypeMACAddress
-	case eos.LLDPChassisIDNetworkAddress().EnumValue():
-		return mapping.ChassisIDTypeNetworkAddress
-	case eos.LLDPChassisIDInterfaceName().EnumValue():
-		return mapping.ChassisIDTypeInterfaceName
-	case eos.LLDPChassisIDLocal().EnumValue():
-		return mapping.ChassisIDTypeLocal
-	}
-	return ""
-}
-
-// Return the OpenConfig port ID type string corresponding to the
-// SNMP port ID type value.
-func lldpPortIDType(id int) string {
-	switch uint32(id) {
-	case eos.LLDPPidInterfaceAlias().EnumValue():
-		return mapping.PortIDTypeInterfaceAlias
-	case eos.LLDPPidPortComponent().EnumValue():
-		return mapping.PortIDTypePortComponent
-	case eos.LLDPPidMacAddress().EnumValue():
-		return mapping.PortIDTypeMACAddress
-	case eos.LLDPPidNetworkAddress().EnumValue():
-		return mapping.PortIDTypeNetworkAddress
-	case eos.LLDPPidInterfaceName().EnumValue():
-		return mapping.PortIDTypeInterfaceName
-	case eos.LLDPPidAgentCircuitID().EnumValue():
-		return mapping.PortIDTypeAgentCircuitID
-	case eos.LLDPPidLocal().EnumValue():
-		return mapping.PortIDTypeLocal
-	}
-	return ""
-}
-
 // There are three kinds of LLDP data: local general (non-port-specific),
 // local per-port (comes with a local interface index), and remote
 // (comes with a local interface index and remote port ID).
@@ -605,7 +469,7 @@ func (s *snmp) handleLldpPDU(pdu gosnmp.SnmpPDU) ([]*gnmi.Update, error) {
 			strval(macFromBytes(pdu.Value.([]byte))))
 	case snmpLldpLocChassisIDSubtype:
 		u = update(GNMILldpStatePath("chassis-id-type"),
-			strval(lldpChassisIDType(pdu.Value.(int))))
+			strval(openconfig.LLDPChassisIDType(pdu.Value.(int))))
 	case snmpLldpLocSysName:
 		u = update(GNMILldpStatePath("system-name"),
 			strval(pdu.Value))
@@ -635,13 +499,13 @@ func (s *snmp) handleLldpPDU(pdu gosnmp.SnmpPDU) ([]*gnmi.Update, error) {
 			strval(pdu.Value))
 	case snmpLldpRemPortIDSubtype:
 		u = update(GNMILldpNeighborStatePath(intfName, remoteID, "port-id-type"),
-			strval(lldpPortIDType(pdu.Value.(int))))
+			strval(openconfig.LLDPPortIDType(pdu.Value.(int))))
 	case snmpLldpRemChassisID:
 		u = update(GNMILldpNeighborStatePath(intfName, remoteID, "chassis-id"),
 			strval(macFromBytes(pdu.Value.([]byte))))
 	case snmpLldpRemChassisIDSubtype:
 		u = update(GNMILldpNeighborStatePath(intfName, remoteID, "chassis-id-type"),
-			strval(lldpChassisIDType(pdu.Value.(int))))
+			strval(openconfig.LLDPChassisIDType(pdu.Value.(int))))
 	case snmpLldpRemSysName:
 		u = update(GNMILldpNeighborStatePath(intfName, remoteID, "system-name"),
 			strval(pdu.Value))
