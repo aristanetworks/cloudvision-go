593d94ae0d107db2ee3de323bda70ee6a69a3bcd
Mateus Pereira <mpereira@arista.com>
2022-06-22 13:51:33 -0700

    devices/openconfig: Add timeout to connection
    If the connection settings are wrong dial may hang forever when using
    WithBlock.
    
    Also adding a Capabilities request right after dial to ensure our connection
    and auth are working.
    
    Change-Id: I59a5b15db4cc7db1555194d028b0e12ce6a86f6b

--- device/devices/openconfig_test.go
+++ device/devices/openconfig_test.go
@@ -5,14 +5,22 @@
 package devices
 
 import (
+	"context"
 	"errors"
+	"net"
 	"strings"
 	"sync"
 	"testing"
 
+	"github.com/aristanetworks/cloudvision-go/device"
 	pg "github.com/aristanetworks/cloudvision-go/provider/gnmi"
 	agnmi "github.com/aristanetworks/goarista/gnmi"
 	"github.com/openconfig/gnmi/proto/gnmi"
+	"github.com/sirupsen/logrus"
+	"golang.org/x/sync/errgroup"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/proto"
 )
 
@@ -205,6 +213,186 @@ func TestOpenConfigDeviceID(t *testing.T) {
 
 			// Wait for DeviceID goroutine to finish
 			wait.Wait()
+		})
+	}
+}
+
+// implements gnmi.GNMIServer
+type mockGnmiServer struct {
+	serveCapability func(ctx context.Context,
+		r *gnmi.CapabilityRequest, s *mockGnmiServer) (*gnmi.CapabilityResponse, error)
+}
+
+func (g *mockGnmiServer) Capabilities(ctx context.Context, r *gnmi.CapabilityRequest) (
+	*gnmi.CapabilityResponse, error) {
+	return g.serveCapability(ctx, r, g)
+}
+
+func (g *mockGnmiServer) Get(_ context.Context, _ *gnmi.GetRequest) (*gnmi.GetResponse, error) {
+	panic("Not implemented")
+}
+
+func (g *mockGnmiServer) Set(_ context.Context, _ *gnmi.SetRequest) (*gnmi.SetResponse, error) {
+	panic("Not implemented")
+}
+
+func (g *mockGnmiServer) Subscribe(_ gnmi.GNMI_SubscribeServer) error {
+	panic("not implemented")
+}
+
+func TestNewOpenConfig(t *testing.T) {
+
+	//
+	nonCompliantServerLst, err := net.Listen("tcp", "localhost:0")
+	if err != nil {
+		t.Fatalf("failed to create gRPC listener: %s", err)
+	}
+	defer nonCompliantServerLst.Close()
+	nonCompliantGrpcServer := grpc.NewServer()
+	defer nonCompliantGrpcServer.Stop()
+	go func() {
+		_ = nonCompliantGrpcServer.Serve(nonCompliantServerLst)
+	}()
+	t.Logf("nonCompliantAddr: %v", nonCompliantServerLst.Addr().String())
+
+	nonGRPCServLst, err := net.Listen("tcp", "localhost:0")
+	if err != nil {
+		t.Fatalf("failed to create gRPC listener: %s", err)
+	}
+	defer nonGRPCServLst.Close()
+	t.Logf("nonGRPCServer: %v", nonGRPCServLst.Addr().String())
+
+	logrus.SetLevel(logrus.DebugLevel)
+
+	for _, tcase := range []struct {
+		name            string
+		opts            map[string]string
+		serveCapability func(ctx context.Context,
+			r *gnmi.CapabilityRequest, s *mockGnmiServer) (*gnmi.CapabilityResponse, error)
+		expectErr string
+	}{
+		{
+			name: "Bad address, can't connect",
+			opts: map[string]string{
+				"address": "wrong:12345",
+				"timeout": "400ms", // so test is quicker
+			},
+			expectErr: "failed to dial: context deadline exceeded",
+		},
+		{
+			name: "Good connection, but auth fails",
+			opts: map[string]string{
+				"address": "ACTUAL_SERVER",
+			},
+			serveCapability: func(ctx context.Context,
+				r *gnmi.CapabilityRequest, s *mockGnmiServer) (*gnmi.CapabilityResponse, error) {
+				return nil, status.Error(codes.Unauthenticated, "auth err")
+			},
+			expectErr: "failed to reach device: rpc error: code = Unauthenticated desc = auth err",
+		},
+		{
+			name: "Can reach address, but it is not compliant to gNMI",
+			opts: map[string]string{
+				"address": nonCompliantServerLst.Addr().String(),
+			},
+			expectErr: "failed to reach device: " +
+				"rpc error: code = Unimplemented desc = unknown service gnmi.gNMI",
+		},
+		{
+			name: "Can reach address, but it is not gRPC",
+			opts: map[string]string{
+				"address": nonGRPCServLst.Addr().String(),
+				"timeout": "400ms",
+			},
+			expectErr: "failed to dial: context deadline exceeded",
+		},
+		{
+			name: "Good connection, capabilities returns non implemented should work",
+			opts: map[string]string{
+				"address": "ACTUAL_SERVER",
+			},
+			serveCapability: func(ctx context.Context,
+				r *gnmi.CapabilityRequest, s *mockGnmiServer) (*gnmi.CapabilityResponse, error) {
+				return nil, status.Error(codes.Unimplemented, "not implemented")
+			},
+		},
+		{
+			name: "Good connection but gets connection refused with code Unavailable, fails",
+			opts: map[string]string{
+				"address": "ACTUAL_SERVER",
+				"timeout": "400ms",
+			},
+			serveCapability: func(ctx context.Context,
+				r *gnmi.CapabilityRequest, s *mockGnmiServer) (*gnmi.CapabilityResponse, error) {
+				return nil, status.Error(codes.Unavailable, "something connect: connection refused")
+			},
+			expectErr: "failed to reach device: " +
+				"rpc error: code = Unavailable desc = something connect: connection refused",
+		},
+		{
+			name: "All good",
+			opts: map[string]string{
+				"address": "ACTUAL_SERVER",
+			},
+			serveCapability: func(ctx context.Context,
+				r *gnmi.CapabilityRequest, s *mockGnmiServer) (*gnmi.CapabilityResponse, error) {
+				return &gnmi.CapabilityResponse{}, nil
+			},
+		},
+	} {
+		t.Run(tcase.name, func(t *testing.T) {
+
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+			errg, _ := errgroup.WithContext(ctx)
+
+			// Setup mock gnmi server
+			server := &mockGnmiServer{
+				serveCapability: tcase.serveCapability,
+			}
+
+			// Start grpc server
+			grpcServer := grpc.NewServer()
+			gRPCLis, err := net.Listen("tcp", "localhost:0")
+			if err != nil {
+				t.Fatalf("failed to create gRPC listener: %s", err)
+			}
+			gnmi.RegisterGNMIServer(grpcServer, server)
+			defer gRPCLis.Close()
+			defer grpcServer.Stop()
+			errg.Go(func() error {
+				return grpcServer.Serve(gRPCLis)
+			})
+
+			// Set actual server address if needed
+			if tcase.opts["address"] == "ACTUAL_SERVER" {
+				tcase.opts["address"] = gRPCLis.Addr().String()
+			}
+
+			// Sanitized options so we don't need to specify what is not required
+			opts, err := device.SanitizedOptions(openConfigOptions(), tcase.opts)
+			if err != nil {
+				t.Fatalf("Bad test options passed: %v", err)
+			}
+
+			dev, err := newOpenConfig(opts)
+			if err != nil {
+				if err.Error() != tcase.expectErr {
+					t.Fatalf("Expected err: %v, got %v", tcase.expectErr, err)
+				}
+			}
+			if err == nil && len(tcase.expectErr) > 0 {
+				t.Fatalf("Expected error %v, but got none", tcase.expectErr)
+			}
+			if len(tcase.expectErr) == 0 && dev == nil {
+				t.Fatalf("Expected a valid device object, got nil")
+			}
+			grpcServer.Stop()
+			cancel()
+
+			if err := errg.Wait(); err != nil {
+				t.Fatal(err)
+			}
 		})
 	}
 }
