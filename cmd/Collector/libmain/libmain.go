cb57029b92712ed0fbcb0d9b696b14b1cec43625
Mateus Pereira <mpereira@arista.com>
2022-12-02 11:20:33 -0800

    cmd/Collector/libmain: Close grpc server when group finishes
    The grpc Serve call does not return on context cancelation,
    so it needs to be stopped manually in order for the program
    to finish.
    
    Change-Id: I6b51002e5640ef5b79fbdce56d2e3afccd69c477

--- cmd/Collector/libmain/libmain.go
+++ cmd/Collector/libmain/libmain.go
@@ -383,17 +383,27 @@ func runMain(ctx context.Context, sc device.SensorConfig) {
 		return watchConfig(*deviceConfigFile, inventory)
 	})
 
+	var gServer *grpc.Server
+	var gListener net.Listener
 	if *grpcAddr != "" {
-		grpcServer, listener, err := newGRPCServer(*grpcAddr, inventory)
+		gServer, gListener, err = newGRPCServer(*grpcAddr, inventory)
 		if err != nil {
 			logrus.Fatalf("Failed to start gRPC server: %v", err)
 		}
-		group.Go(func() error { return grpcServer.Serve(listener) })
+		go func() {
+			if err := gServer.Serve(gListener); err != nil {
+				logrus.Infof("grpc server returned error: %v", err)
+			}
+		}()
 	}
 
 	logrus.Info("Collector is running")
 	if err := group.Wait(); err != nil {
 		logrus.Fatalf("group returned with error: %v", err)
+	}
+	if gServer != nil {
+		gServer.Stop()
+		_ = gListener.Close()
 	}
 	logrus.Infof("Collector is finished")
 }
