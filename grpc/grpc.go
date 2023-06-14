1587575f920291db7e4d8d02bd5271e837414f45
Pengyu Che <cpy@arista.com>
2023-06-14 12:23:11 -0700

    Revert "grpc, internal/redirector: Resource API Redirection client"
    This reverts commit 48a74d90b5d54454ade859d4592d6ae50a2eaf1c.
    
    Reason for revert: Temporarily revert for solving resource API conflicts
    
    Change-Id: I779d3e86c9d30fc4a91a205a1456e6c810fcd0e8

--- grpc/grpc.go
+++ grpc/grpc.go
@@ -7,18 +7,11 @@ package grpc
 
 import (
 	"context"
-	"errors"
 	"fmt"
-	"os"
-	"strings"
 
-	"github.com/aristanetworks/cloudvision-go/internal/redirector"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 )
-
-// ErrNoClusterTargets is returned when no clusters are returned.
-var ErrNoClusterTargets = errors.New("no redirection cluster targets")
 
 // DialWithAuth dials a gRPC endpoint, target, with the provided
 // authentication config and dial options.
@@ -30,12 +23,6 @@ func DialWithAuth(ctx context.Context, target string, auth *Auth, opts ...grpc.D
 		return nil, fmt.Errorf("failed to configuration authentication scheme: %s", err)
 	}
 	opts = append(opts, grpc.WithBlock())
-
-	target, err = resolveRedirection(ctx, target, opts...)
-	if err != nil {
-		return nil, fmt.Errorf("unable to redirect: %w", err)
-	}
-
 	return grpc.DialContext(ctx, target, append(opts, authOpts...)...)
 }
 
@@ -49,41 +36,5 @@ func DialWithToken(ctx context.Context, target, token string, opts ...grpc.DialO
 		grpc.WithPerRPCCredentials(NewAccessTokenCredential(token)),
 		grpc.WithTransportCredentials(credentials.NewTLS(TLSConfig())),
 	)
-
-	target, err := resolveRedirection(ctx, target, opts...)
-	if err != nil {
-		return nil, fmt.Errorf("unable to redirect: %w", err)
-	}
-
 	return grpc.DialContext(ctx, target, opts...)
-}
-
-// resolveRedirection uses the resolver to to get the regional endpoint. If the redirection
-// fails we just return the client specified endpoint itself.
-func resolveRedirection(
-	ctx context.Context, target string, opts ...grpc.DialOption,
-) (string, error) {
-	// Allow the client to disable redirection
-	if strings.ToLower(os.Getenv("CLOUDVISION_REGIONAL_REDIRECT")) == "false" {
-		return target, nil
-	}
-
-	targets, err := redirector.Resolve(ctx, target, opts...)
-	if err != nil {
-		// Special case when we get back the Unimplemented error. This means
-		// that the functionality is not yet available serverside and we should
-		// just return the original target.
-		if errors.Is(err, redirector.ErrUnimplemented) {
-			return target, nil
-		}
-
-		return "", err
-	}
-
-	// Prevent a panic if targets is a nil slice and return an error
-	// instead.
-	if len(targets) < 1 || len(targets[0]) < 1 {
-		return "", ErrNoClusterTargets
-	}
-	return targets[0][0], nil
 }
