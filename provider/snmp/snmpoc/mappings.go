6653ead6bdabf634195db3cad5528c08bf0026e7
Charishma B <charishma@arista.com>
2024-03-07 09:45:01 +0000

    Revert "provider/snmp: Update ocComponentType enum map"
    This reverts commit 2ff6beab033875798a1665d3142b96de8ac1c360.
    
    Reason for revert: Updating this enum mappings is causing issues in sending Set Requests for components as we do not have the equivalent type mapping in standard openconfig platform types
    https://github.com/openconfig/public/blob/master/release/models/platform/openconfig-platform-types.yang
    
    time="2024-03-07T09:04:42+05:30" level=error msg="Error in sendUpdates: rpc error: code = InvalidArgument desc = failed while setting /components/component[name=1008]/state/type: failed while unmarshaling Leaf type: \"openconfig-platform-types:CONTAINER\": error decoding JSON value for node type: value openconfig-platform-types:CONTAINER not suitable for any union types: [identityref identityref]" file="libmain/base_monitor.go:24" Device=cmd-device
    
    Change-Id: Ic14696802dc659ecbb83d02d364f9975dcad8b1a

--- provider/snmp/snmpoc/mappings.go
+++ provider/snmp/snmpoc/mappings.go
@@ -884,17 +884,14 @@ func processEntPhysicalTableVal(oid string, pdu *gosnmp.SnmpPDU) (interface{}, e
 		2:  "",
 		3:  "CHASSIS",
 		4:  "BACKPLANE",
-		5:  "CONTAINER",
+		5:  "",
 		6:  "POWER_SUPPLY",
 		7:  "FAN",
 		8:  "SENSOR",
-		9:  "MODULE",
+		9:  "",
 		10: "PORT",
-		11: "STACK",
+		11: "",
 		12: "CPU",
-		13: "ENERGY_OBJECT",
-		14: "BATTERY",
-		15: "STORAGE_DRIVE",
 	}
 	class, ok := ocComponentTypeMap[v]
 	if !ok {
